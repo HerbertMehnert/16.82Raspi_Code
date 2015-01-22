6290f4ded08a352e878e84b2dbf88edd2c9fbb23
HerbertMehnert <hfmehnert@gmail.com>
2015-01-22 22:25:46 +0000

    Added MotorTesting directory.

---
+++ C++/Examples/MotorTesting/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Navio/PCA9685.h"
+
+#define NAVIO_RCOUTPUT_1 3
+#define NAVIO_RCOUTPUT_2 4
+
+
+using namespace std;
+
+int main()
+{
+    PCA9685 pwm;
+    pwm.initialize():
+    pwm.setFrequency(50);
+
+    while(true){
+
+        pwm.setPWM(NAVIO_RCOUTPUT_1, 500, 1012);
+
+    }
+    return 0;
+}
